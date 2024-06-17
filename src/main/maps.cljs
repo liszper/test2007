06507aaea39edd0ef44e1b69116f809e879f81c6
Ròbert Áron Zawiasa <raz@Roberts-MacBook-Air.local>
2024-06-17 19:33:27 +0200

    update

--- src/main/maps.cljs
+++ src/main/maps.cljs
@@ -79,22 +79,26 @@
    {:gltf 
     {:castShadow "castShadow" 
      :receiveShadow "receiveShadow"
-     :position [0 -30 -30]
+     :position [0 0 0]
      ;:rotation [(/ (- (.-PI js/Math)) 2) 0 0]
-     :scale 10
-     :src "/maps/small_cybertown.glb"} 
+     :scale 95
+     :src "/maps/untitled.glb"} 
     :control {:maxVelLimit 5
-              :sprintMult 2
-              :jumpVel 9
+              :sprintMult 3
+              :jumpVel 5
               ;:airDragMultiplier 0.1
               ;:fallingGravityScale 10
               :camInitDis -3}
     ;:skybox [:> drei/Stars]
     ;:skybox [:> drei/Environment {:files "/psy.hdr" :ground {:scale 100}}]
     :lights [:<>
-       [:directionalLight {:intensity 1 :castShadow "castShadow" :position [0 10 0] :shadow-bias -0.0004}
-        [:orthographicCamera {:args [-20 20 20 -20] :attach "shadow-camera"}]]
-       [:ambientLight {:intensity 30}]
+       ;[:directionalLight {:intensity 1 :castShadow "castShadow" :position [0 10 0] :shadow-bias -0.0004}
+       ; [:orthographicCamera {:args [-20 20 20 -20] :attach "shadow-camera"}]]
+     ;  [:ambientLight {:intensity 100}]
+     [:pointLight {:decay 0 :intensity 2 :position [100 10 -100]}]
+     [:pointLight {:decay 0 :intensity 2 :position [-100 10 100]}]
+     [:pointLight {:decay 0 :intensity 2 :position [100 10 100]}]
+     [:pointLight {:decay 0 :intensity 2 :position [-100 10 -100]}]
              
              
              ]
